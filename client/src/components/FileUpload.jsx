2a9ae9759486e0b940d0173f339fb12494b1fa9a
Ben Deltenre <benjdelt@gmail.com>
2019-08-28 15:45:23 -0700

    wip

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
+// import nodejs bindings to native tensorflow,
+// not required, but will speed up things drastically (python required)
+// import '@tensorflow/tfjs-node';
+
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+import * as canvas from 'canvas';
+
+import * as faceapi from 'face-api.js';
+
+
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement, additionally an implementation
+// of ImageData is required, in case you want to use the MTCNN
+const { Canvas, Image, ImageData } = canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+console.log(faceapi.nets);
+const path = './models';
+    Promise.all([
+      faceapi.nets.faceRecognitionNet.loadFromUri(path),
+      faceapi.nets.faceLandmark68Net.loadFromUri(path),
+      faceapi.nets.ssdMobilenetv1.loadFromUri(path)
+    ]).then(() => {
+      console.log("Something")
+      // this.setState({loadingModels: false})
+    }).catch(err => console.log(err))
+
 class FileUpload extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      imageURL: '',
+      imageURL: 'http://localhost:3001/public/image.jpg',
+      loadingModels: true,
     };
 
     this.handleUploadImage = this.handleUploadImage.bind(this);
@@ -24,6 +51,10 @@ class FileUpload extends React.Component {
         this.setState({ imageURL: `http://localhost:3001/${body.file}` });
       });
     });
+  }
+
+  componentDidMount = () => {
+    
   }
 
   render() {
